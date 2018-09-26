889ccbd70cb47d51052e97012525a3c61e3a1c86
Pim <pimgroeneveld92@gmail.com>
2018-09-26 10:23:21 +0100

    hottubArea getter +tests

--- src/main/java/HottubArea.java
+++ src/main/java/HottubArea.java
@@ -1,2 +1,24 @@
 public class HottubArea {
+
+    private int roomNumber;
+    private String type;
+    private double rate;
+
+    public HottubArea(int roomNumber, String type, double rate){
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.rate = rate;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getRate() {
+        return rate;
+    }
 }
--- src/test/java/HottubAreaTest.java
+++ src/test/java/HottubAreaTest.java
@@ -1,5 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HottubAreaTest {
 
+    HottubArea hottubArea;
+
+    @Before
+    public void before(){
+        hottubArea = new HottubArea(10, "double", 59.99);
+
+    }
+
+    @Test
+    public void getRoomNumber() {
+        assertEquals(10, hottubArea.getRoomNumber());
+    }
+
+    @Test
+    public void getType() {
+        assertEquals("double", hottubArea.getType());
+    }
+
+    @Test
+    public void getRate() {
+        assertEquals(59.99, hottubArea.getRate(), 0.0);
+    }
 }
